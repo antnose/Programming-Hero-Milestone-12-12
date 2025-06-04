4edcbda00eefbf6b579c864940bd85ed1f5d6ff9
antnose <antnose28@gmail.com>
2025-06-04 23:16:36 +0600

    add dynamic rating or star in Testimonials section

---
+++ client/src/pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,50 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+// import required modules
+import { Navigation } from "swiper/modules";
+import { useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+const Testimonial = () => {
+  const [reviews, setReviews] = useState([]);
+  const [rating, setRating] = useState(0);
+
+  useState(() => {
+    fetch(`reviews.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, []);
+  console.log(reviews);
+
+  return (
+    <section className="my-20">
+      <SectionTitle
+        heading={"Testimonials"}
+        subHeading={"What Our client Say"}
+      />
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="m-24">
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <p>{review.details}</p>
+              <h3 className="text-2xl text-orange-400">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonial;
