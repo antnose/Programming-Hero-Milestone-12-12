22d7b499e4e16cba6ef4f88eb30f6dec48fdce17
antnose <antnose28@gmail.com>
2025-06-05 17:07:11 +0600

    using helmet change the title

---
+++ client/src/pages/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro | Menu</title>
+      </Helmet>
+      <h1>Menue Page</h1>
+    </div>
+  );
+};
+
+export default Menu;
