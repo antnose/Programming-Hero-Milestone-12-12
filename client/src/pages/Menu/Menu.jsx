bfb68f355f0ef0df498df0a687f5079e8369e606
antnose <antnose28@gmail.com>
2025-06-07 20:45:47 +0600

    almost done menue section

--- client/src/pages/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu.jsx
@@ -1,14 +1,39 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import Cover from "../Shared/Cover/Cover";
 import menuImg from "../../assets/menu/banner3.jpg";
+import pizzaImg from "../../assets/menu/pizza-bg.jpg";
+import soupImg from "../../assets/menu/soup-bg.jpg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+import useMenu from "../../hooks/useMenu";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import MenuCategory from "./MenuCategory/MenuCategory";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
     <div>
       <Helmet>
         <title>Bistro | Menu</title>
       </Helmet>
       <Cover img={menuImg} title={"Our Menu"} />
+      {/* main cover */}
+      <SectionTitle heading={`Today's Offer`} subHeading={`Don't Miss`} />
+      {/* offered menu items */}
+      <MenuCategory items={offered} />
+      {/* dessert menu items */}
+      <MenuCategory items={dessert} title={`Deesert`} coverImg={dessertImg} />
+
+      <MenuCategory items={pizza} title={`Pizza`} coverImg={pizzaImg} />
+
+      <MenuCategory items={soup} title={`Soup`} coverImg={soupImg} />
+
+      <MenuCategory items={salad} title={`Salad`} coverImg={saladImg} />
     </div>
   );
 };
